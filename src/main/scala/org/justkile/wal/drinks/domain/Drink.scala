d4794dbbf9696fb95ae498ba6d1c65d682109a2b
Ben Lenser <ben.lenser@immobilienscout24.de>
2018-12-30 20:55:49 +0100

    Move Drink out of DrinkType

---
+++ src/main/scala/org/justkile/wal/drinks/domain/Drink.scala
@@ -0,0 +1,5 @@
+package org.justkile.wal.drinks.domain
+
+import org.justkile.wal.drinks.domain.DrinkType.DrinkType
+
+case class Drink(id: Int, name: String, `type`: DrinkType)
--- src/main/scala/org/justkile/wal/drinks/domain/DrinkType.scala
+++ src/main/scala/org/justkile/wal/drinks/domain/DrinkType.scala
@@ -2,21 +2,17 @@ package org.justkile.wal.drinks.domain
 
 import doobie.util.meta.Meta
 import io.circe.{Decoder, Encoder}
-import org.justkile.wal.drinks.domain.DrinkType.DrinkType
 
 object DrinkType extends Enumeration {
   type DrinkType = Value
   val COCKTAIL = Value("COCKTAIL")
   val SHOT = Value("SHOT")
   val BEER = Value("BEER")
-  val COFFEE = Value("COFFEE")
   val SOFTDRINK = Value("SOFTDRINK")
 
   implicit val drinkTypeMeta: Meta[DrinkType] = Meta[String]
     .xmap((s: String) => DrinkType.withName(s), (d: DrinkType) => d.toString)
 
-  implicit val genderDecoder: Decoder[DrinkType] = Decoder.enumDecoder(DrinkType)
-  implicit val genderEncoder: Encoder[DrinkType] = Encoder.enumEncoder(DrinkType)
+  implicit val drinkTypeDecoder: Decoder[DrinkType] = Decoder.enumDecoder(DrinkType)
+  implicit val drinkTypeEncoder: Encoder[DrinkType] = Encoder.enumEncoder(DrinkType)
 }
-
-case class Drink(id: Int, name: String, `type`: DrinkType)
