53280248600f11ddda1ba30119804d3e70585c9e
Ben Lenser <ben.lenser@immobilienscout24.de>
2018-12-30 18:43:42 +0100

    Rewrite to use pure from applicative

--- src/main/scala/org/justkile/wal/event_sourcing/event_bus/EventBus.scala
+++ src/main/scala/org/justkile/wal/event_sourcing/event_bus/EventBus.scala
@@ -1,6 +1,6 @@
 package org.justkile.wal.event_sourcing.event_bus
 
-import cats.Traverse
+import cats.Applicative
 import cats.effect.Sync
 import cats.implicits._
 import io.chrisdavenport.log4cats.Logger
@@ -8,24 +8,23 @@ import org.justkile.wal.utils.Done
 
 import scala.reflect.ClassTag
 
-class EventBus[F[_]: Sync: Logger] {
+class EventBus[F[_]: Sync: Logger: Applicative] {
 
   import org.justkile.wal.event_sourcing.event_bus.EventBus.EventHandler
 
   lazy val eventHandlerRegistry = new Registry[F]
 
-  def subscribe[T](e: EventHandler[F, T])(implicit c: ClassTag[T]): F[Done] = {
+  def subscribe[T](e: EventHandler[F, T])(implicit c: ClassTag[T], F: Applicative[F]): F[Done] = {
     for {
-      _ <- Logger[F].info("Subscribe event handler")
-      _ = eventHandlerRegistry.registerEventHandler[T](e)
+      _ <- F.pure(eventHandlerRegistry.registerEventHandler[T](e))
     } yield Done
   }
 
-  def publish[E](event: E): F[Done] = {
+  def publish[E](event: E)(implicit F: Applicative[F]): F[Done] = {
     for {
       _ <- Logger[F].info(s"Publish event $event")
-      executedEventHandlers = eventHandlerRegistry.lookUpEventHandler(event).map(_.handle(event))
-      _ <- Traverse[List].sequence(executedEventHandlers)
+      executedEventHandlers <- F.pure(eventHandlerRegistry.lookUpEventHandler(event).map(_.handle(event)))
+      _ <- executedEventHandlers.sequence
     } yield Done
   }
 }
