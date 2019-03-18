c556bdbf005231c793bde20ab02cdd64d4f3f1f3
Ben Lenser <ben.lenser@immobilienscout24.de>
2019-03-18 20:03:05 +0100

    Add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import Accounting from './components/accounting';
+import Home from './components/home';
 import getStore from './redux';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <Provider store={getStore()}>
-        <Accounting />
+        <Router>
+          <Route path="/" exact component={Home} />
+          <Route path="/abrechnung" component={Accounting} />
+        </Router>
       </Provider>
     );
   }
---
+++ frontend/src/components/home/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Accounting extends Component {
+  render() {
+    return (
+      <div>
+        FOoo
+
+      </div>
+    );
+  }
+}
