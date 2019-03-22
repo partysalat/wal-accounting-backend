3f15f2ce1d7556aacbf835f238ab014f74aab70c
Ben Lenser <ben.lenser@immobilienscout24.de>
2019-03-22 13:36:47 +0100

    Add responsive design

--- frontend/src/components/home/bestlist/index.js
+++ frontend/src/components/home/bestlist/index.js
@@ -1,15 +1,9 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import { appendNewsItems, loadBestlist, subscribeToNewsUpdate } from '../../../redux/actions';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import Avatar from '@material-ui/core/Avatar';
 import Tooltip from '@material-ui/core/Tooltip';
-
+import {loadBestlist} from '../../../redux/actions';
+import './bestlist.css';
 
 class Bestlist extends Component {
   componentWillMount() {
@@ -18,38 +12,35 @@ class Bestlist extends Component {
 
   render() {
     return (
-      <Table >
-        <TableHead>
-          <TableRow>
-            <TableCell>No.</TableCell>
-            <TableCell align="right">Name</TableCell>
-            <TableCell align="right">Biere</TableCell>
-            <TableCell align="right">Cocktails</TableCell>
-            <TableCell align="right">Shots</TableCell>
-            <TableCell align="right">Softdrinks</TableCell>
-            <TableCell align="right">Achievements</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {this.props.bestlist.map((row, index) => (
-            <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
-                {index + 1}.
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {row.user.name}
-              </TableCell>
-              <TableCell align="right">{row.beerCount}</TableCell>
-              <TableCell align="right">{row.cocktailCount}</TableCell>
-              <TableCell align="right">{row.shotCount}</TableCell>
-              <TableCell align="right">{row.softdrinkCount}</TableCell>
-              <TableCell align="right">{row.achievements.map(payload => (<Tooltip key={payload.id} title={`${payload.name}: ${payload.description}`}>
-                <Avatar alt={payload.name} src={payload.imagePath} sizes="xs"/>
-              </Tooltip>))}</TableCell>
-            </TableRow>
-                ))}
-        </TableBody>
-      </Table>
+      <table>
+        <tr>
+          <th>No.</th>
+          <th>Name</th>
+          <th>Biere</th>
+          <th>Cocktails</th>
+          <th>Shots</th>
+          <th>Softdrinks</th>
+          <th>Achievements</th>
+        </tr>
+        {this.props.bestlist.map((row, index) => (
+          <tr key={row.id}>
+            <td component="th" scope="row">
+              {index + 1}.
+            </td>
+            <td component="th" scope="row">
+              {row.user.name}
+            </td>
+            <td align="right">{row.beerCount}</td>
+            <td align="right">{row.cocktailCount}</td>
+            <td align="right">{row.shotCount}</td>
+            <td align="right">{row.softdrinkCount}</td>
+            <td align="right">{row.achievements.map(payload => (
+              <Tooltip key={payload.id} title={`${payload.name}: ${payload.description}`}>
+                <Avatar className="avatar-small" alt={payload.name} src={payload.imagePath} sizes="xs"/>
+              </Tooltip>))}</td>
+          </tr>
+        ))}
+      </table>
     );
   }
 }
--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 import Bestlist from './bestlist';
 import Feed from './feed';
 import './home.css';
@@ -7,9 +11,19 @@ import './home.css';
 export default function Accounting() {
   return (
     <div>
-      <Grid container>
-        <Grid item xs={8}><Bestlist /></Grid>
-        <Grid item xs={4}><Feed /></Grid>
+
+      <Grid container spacing={16}>
+        <Grid item md={12} sm={12}>
+          <AppBar position="static" color="default">
+            <Toolbar>
+              <Typography variant="h6" color="inherit">
+              Photos
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Grid>
+        <Grid item md={8} sm={12}><Bestlist /></Grid>
+        <Grid item md={4} sm={12}><Feed /></Grid>
       </Grid>
 
     </div>
