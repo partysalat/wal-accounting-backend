ea03206dd2c2df654116a86776bfa8d5c45090f1
Ben Lenser <ben.lenser@immobilienscout24.de>
2019-10-15 20:49:29 +0200

    Implement async event handlers

--- src/main/scala/org/justkile/wal/event_sourcing/event_bus/EventBus.scala
+++ src/main/scala/org/justkile/wal/event_sourcing/event_bus/EventBus.scala
@@ -1,32 +1,14 @@
 package org.justkile.wal.event_sourcing.event_bus
 
-import cats.Applicative
-import cats.effect.Sync
-import cats.implicits._
-import io.chrisdavenport.log4cats.Logger
+import org.justkile.wal.event_sourcing.event_bus.EventBus.EventHandler
 import org.justkile.wal.utils.Done
 
 import scala.reflect.ClassTag
 
-class EventBus[F[_]: Sync: Logger: Applicative] {
+trait EventBus[F[_]] {
 
-  import org.justkile.wal.event_sourcing.event_bus.EventBus.EventHandler
-
-  lazy val eventHandlerRegistry = new Registry[F]
-
-  def subscribe[T](e: EventHandler[F, T])(implicit c: ClassTag[T], F: Applicative[F]): F[Done] = {
-    for {
-      _ <- F.pure(eventHandlerRegistry.registerEventHandler[T](e))
-    } yield Done
-  }
-
-  def publish[E](event: E)(implicit F: Applicative[F]): F[Done] = {
-    for {
-      _ <- Logger[F].info(s"Publish event $event")
-      executedEventHandlers <- F.pure(eventHandlerRegistry.lookUpEventHandler(event).map(_.handle(event)))
-      _ <- executedEventHandlers.sequence
-    } yield Done
-  }
+  def subscribe[T: ClassTag](e: EventHandler[F, T]): F[Done]
+  def publish[E](event: E): F[Done]
 }
 
 object EventBus {
--- src/main/scala/org/justkile/wal/event_sourcing/event_bus/EventBusIO.scala
+++ src/main/scala/org/justkile/wal/event_sourcing/event_bus/EventBusIO.scala
@@ -1,9 +1,30 @@
 package org.justkile.wal.event_sourcing.event_bus
 
 import cats.effect.IO
+import io.chrisdavenport.log4cats.Logger
+import org.justkile.wal.utils.Done
 import org.justkile.wal.utils.LoggerIO._
+import cats.implicits._
+
+import scala.reflect.ClassTag
 
 object EventBusIO {
 
-  implicit val eventBusIO: EventBus[IO] = new EventBus[IO]
+  implicit val eventBusIO: EventBus[IO] = new EventBus[IO] {
+
+    lazy val eventHandlerRegistry = new Registry[IO]
+
+    override def subscribe[T: ClassTag](e: EventBus.EventHandler[IO, T]): IO[Done] =
+      for {
+        _ <- IO.pure(eventHandlerRegistry.registerEventHandler[T](e))
+      } yield Done
+
+    override def publish[E](event: E): IO[Done] =
+      for {
+        _ <- Logger[IO].info(s"Publish event $event")
+        executedEventHandlers <- IO.pure(eventHandlerRegistry.lookUpEventHandler(event).map(_.handle(event)))
+        _ = executedEventHandlers.sequence.unsafeRunAsyncAndForget()
+      } yield Done
+
+  }
 }
